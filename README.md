af28d12f838ca6bd28784fa10df5187154cbef4b
Jeff Escalante <jescalan@users.noreply.github.com>
2025-06-04 11:48:18 -0400

    update to support SSE and streamable http

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Minimal MCP Server Example
 
-The simplest, most bare bones implementation of an MCP server, using express. Run with `npm start`.
+The simplest, most bare bones implementation of an MCP server, using express. It exposes endpoints for both SSE and Streamable HTTP. Run with `npm start`.
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
 import { z } from "zod";
 
 const server = new McpServer({
@@ -8,22 +9,42 @@ const server = new McpServer({
   version: "0.0.1",
 });
 
-server.tool("say-hello", { name: z.string() }, async ({ name }) => ({
-  content: [{ type: "text", text: `Hello ${name}!` }],
-}));
+server.tool(
+  "roll_dice",
+  "Rolls an N-sided die",
+  { sides: z.number().int().min(2) },
+  async ({ sides }) => {
+    const value = 1 + Math.floor(Math.random() * sides);
+    return {
+      content: [{ type: "text", text: `🎲 You rolled a ${value}!` }],
+    };
+  }
+);
 
 const app = express();
-let transport: SSEServerTransport;
 
-// initialize connection and session
-app.get("/mcp/register", async (_, res) => {
-  transport = new SSEServerTransport("/mcp/message", res);
-  await server.connect(transport);
+let sseTransport: SSEServerTransport;
+
+// SSE transport handling
+app.get("/sse", async (_, res) => {
+  sseTransport = new SSEServerTransport("/sse/message", res);
+  await server.connect(sseTransport);
   console.log("connected");
 });
 
-app.post("/mcp/message", async (req, res) => {
-  await transport.handlePostMessage(req, res);
+app.post("/sse/message", async (req, res) => {
+  await sseTransport.handlePostMessage(req, res);
+});
+
+// Streamable HTTP transport handling
+app.post("/mcp", express.json(), async (req, res) => {
+  const transport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: undefined,
+  });
+
+  await server.connect(transport);
+
+  await transport.handleRequest(req, res, req.body);
 });
 
 app.listen(3000);
